f7fe9a2b7d6c291eb7e8c5c1ad1749191023129e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2023-11-04 10:28:37 +0000

    Show type of unrepresentable types for data_regression

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,12 @@
 # 3rd party
+from pytest_regressions.data_regression import RegressionYamlDumper
+from yaml.representer import RepresenterError
 
 pytest_plugins = ("coincidence", )
+
+
+def represent_undefined(self, data):  # noqa: MAN001,MAN002
+	raise RepresenterError("cannot represent an object", data, type(data))
+
+
+RegressionYamlDumper.represent_undefined = represent_undefined  # type: ignore[assignment]
