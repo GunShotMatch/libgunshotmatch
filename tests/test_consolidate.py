52d9dd3a1b387f84e150c7ca63fe5a9c19467b01
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2024-02-26 16:06:52 +0000

    Increase test coverage.

--- tests/test_consolidate.py
+++ tests/test_consolidate.py
@@ -1,9 +1,11 @@
 # stdlib
+from operator import attrgetter
 from typing import Optional
 
 # 3rd party
 import numpy
 import pyms_nist_search
+import pytest
 from coincidence.regressions import AdvancedDataRegressionFixture, _representer_for
 from domdf_python_tools.paths import PathPlus
 from domdf_python_tools.typing import PathLike
@@ -45,7 +47,8 @@ class MockEngine(pyms_nist_search.Engine):
 
 
 def test_consolidate(
-		advanced_data_regression: AdvancedDataRegressionFixture, dataframe_regression: DataFrameRegressionFixture
+		advanced_data_regression: AdvancedDataRegressionFixture,
+		dataframe_regression: DataFrameRegressionFixture,
 		):
 	project_file = PathPlus(__file__).parent / "ELEY .22LR.gsmp"
 	project = Project.from_file(project_file)
@@ -69,6 +72,47 @@ def test_consolidate(
 		consolidated_peaks.append(cp_as_dict)
 	advanced_data_regression.check(consolidated_peaks)
 	dataframe_regression.check(ms_comparison_df, basename="test_consolidate_ms_comparison_df")
+
+
+def test_consolidated_peak_attributes():
+
+	project = Project.from_file("tests/Eley Hymax.gsmp")
+	assert project.consolidated_peaks is not None
+
+	peaks = sorted(project.consolidated_peaks, key=attrgetter("area"), reverse=True)
+	largest_peak = peaks[0]
+	assert largest_peak.rt == 1276.2899780273438
+	assert largest_peak.rt_stdev == 0.8302141496232701
+	assert largest_peak.area == 7313500780.940475
+	assert largest_peak.area_stdev == 509223188.0045349
+	assert largest_peak.average_ms_comparison == 999.775888623915
+	assert largest_peak.ms_comparison_stdev == 0.16859417266292104
+
+	top_hit = largest_peak.hits[0]
+	assert top_hit.match_factor == 927.25
+	assert top_hit.match_factor_stdev == 6.139014578904337
+	assert top_hit.reverse_match_factor == 929.0
+	assert top_hit.reverse_match_factor_stdev == 6.519202405202649
+	assert top_hit.average_hit_number == 1.0
+	assert len(top_hit) == 4
+
+	seventh_hit = largest_peak.hits[6]
+	assert seventh_hit.match_factor == 643.75
+	assert seventh_hit.match_factor_stdev == 3.491060010942235
+	assert seventh_hit.reverse_match_factor == 652.5
+	assert seventh_hit.reverse_match_factor_stdev == 3.640054944640259
+	assert seventh_hit.average_hit_number == 7.75
+	assert seventh_hit.hit_number_stdev == 0.82915619758885
+	assert len(seventh_hit) == 4
+
+	bottom_hit = largest_peak.hits[-1]
+	assert bottom_hit.match_factor == 626.0
+	assert bottom_hit.match_factor_stdev == 0.0
+	assert bottom_hit.reverse_match_factor == 647.0
+	assert bottom_hit.reverse_match_factor_stdev == 0.0
+	assert bottom_hit.average_hit_number == 10.0
+	assert bottom_hit.hit_number_stdev == 0.0
+	assert len(bottom_hit) == 1
 
 
 def test_pairwise_ms_comparison(dataframe_regression: DataFrameRegressionFixture):
